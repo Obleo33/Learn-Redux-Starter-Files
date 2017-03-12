33d1ebb435fa14a8034982d1b85ce7eaf4cb09d9
Franklin Crosby <obleo33@gmail.com>
2017-03-12 13:21:32 -0600

    complete tutorial

---
+++ learn-redux/client/reducers/posts.js
@@ -0,0 +1,21 @@
+// reducer takes in 2 things:
+// 1. the action (what happened)
+// 2. copy of the current state
+
+
+function posts(state = [], action) {
+  switch(action.type) {
+    case 'INCREMENT_LIKES' :
+      console.log('incrementing likes');
+      const i = action.index;
+      return [
+        ...state.slice(0,i),
+        {...state[i], likes: state[i].likes + 1},
+        ...state.slice(i+1),
+      ]
+    default:
+      return state;
+  }
+}
+
+export default posts
